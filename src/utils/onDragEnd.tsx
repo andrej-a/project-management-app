95217afb9a661854a2e1121ac1446c6083089bb8
Anastasia Polivoda <stacy99anastasia@gmail.com>
2022-11-22 20:47:51 +0300

    fix: drag and drop logic

--- src/utils/onDragEnd.tsx
+++ src/utils/onDragEnd.tsx
@@ -2,30 +2,47 @@ import { Dispatch } from '@reduxjs/toolkit';
 import { DropResult } from 'react-beautiful-dnd';
 import { IColumn } from '../models/IColumn';
 import { ITask } from '../models/ITask';
-import { setColumns, updateTaskOrder } from '../slices/boardSlice/boardSlice';
-import { swapElements } from './utils';
+import { setColumns, setTasks } from '../slices/boardSlice/boardSlice';
+import { reorderElements, swapElements } from './utils';
 
 export const onDragEnd =
   (dispatch: Dispatch, tasks: ITask[], columns: IColumn[]) => (result: DropResult) => {
-    if (result.destination) {
-      switch (result.type) {
+    const { source, destination, type } = result;
+    if (
+      destination &&
+      !(destination.droppableId === source.droppableId && destination.index === source.index)
+    ) {
+      switch (type) {
         case 'column':
-          dispatch(
-            setColumns(swapElements(columns, result.destination.index, result.source.index))
-          );
+          dispatch(setColumns(swapElements(columns, destination.index, source.index)));
           break;
 
         case 'task':
-          const task = tasks.find((task) => task._id === result.draggableId);
-          if (task)
+          if (destination.droppableId === source.droppableId) {
+            const columnId = destination.droppableId;
+            const column = [...tasks.filter((task) => task.columnId === columnId)];
+            const restColumns = [...tasks.filter((task) => task.columnId !== columnId)];
+
             dispatch(
-              updateTaskOrder({
-                index: tasks.indexOf(task),
-                order: result.destination.index,
-                columnId: result.destination.droppableId,
-              })
+              setTasks([...restColumns, ...swapElements(column, destination.index, source.index)])
             );
-          break;
+          } else {
+            const newColumnId = destination.droppableId;
+            const oldColumnId = source.droppableId;
+            const newColumn = [...tasks.filter((task) => task.columnId === newColumnId)];
+            const oldColumn = [...tasks.filter((task) => task.columnId === oldColumnId)];
+            const restColumns = [
+              ...tasks.filter(
+                (task) => task.columnId !== newColumnId && task.columnId !== oldColumnId
+              ),
+            ];
+            const [removed] = oldColumn.splice(source.index, 1);
+            newColumn.splice(destination.index, 0, { ...removed, columnId: newColumnId });
+            reorderElements(newColumn);
+            reorderElements(oldColumn);
+            return dispatch(setTasks([...restColumns, ...newColumn, ...oldColumn]));
+          }
       }
-    }
+      return;
+    } else return;
   };
