3893430c52e410d40ccaa712d8578b5bc66ef7d3
Lizaveta Petrova <goldglaider@gmail.com>
2022-11-30 21:30:07 +0300

    refactor: add some changes to profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,15 +1,9 @@
-import { useAppSelector } from '../../hooks/hooks';
 import { EditProfileForm } from './Form/EditProfileForm';
 import { Wrapper } from './ProfilePage.styled';
 const ProfilePage = () => {
-  const { dictionary } = useAppSelector((state) => {
-    return {
-      dictionary: state.language,
-    };
-  });
   return (
     <Wrapper>
-      <EditProfileForm></EditProfileForm>
+      <EditProfileForm />
     </Wrapper>
   );
 };
