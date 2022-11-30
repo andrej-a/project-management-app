6f42e4a5eec53e91bb1d90ce5e05b0423116b793
Lizaveta Petrova <goldglaider@gmail.com>
2022-11-30 17:11:07 +0300

    fix: open-close burger and show switcher for unathorized user

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -4,10 +4,7 @@ import { HandySvg } from 'handy-svg';
 import { useAppSelector, useAppDispatch } from '../../../hooks/hooks';
 import { localData, Styles, SvgSizes } from '../../../constants/applicationConstants';
 
-import { langChange } from '../../../slices/languageSlice/languageSlice';
-import { EnglishState, RussianState } from '../../../slices/languageSlice/initialState';
 import { authorizationSwitch } from '../../../slices/userSlice/userSlice';
-
 import { setStatus } from '../../../slices/modalsSlice/modalsSlice';
 
 import { getCookie } from '../../../utils/cookie/getCookie';
@@ -27,21 +24,25 @@ import {
   TextContainer,
   ItemContainer,
 } from './Navigation.styled';
+import { ButtonContainer, SignIn, SignUp } from '../Header.styled';
+
 import { langEnum, localStorageEnum, themeEnum } from '../../../constants/localStorage';
 import { updateTheme } from '../../../utils/updateTheme';
 import { updateLang } from '../../../utils/updateLang';
 import { deleteValueFromLocalStorage } from '../../../utils/deleteValueFromLocalStorage';
 
 type props = {
-  isOpen: boolean;
+  isMenuOpen: boolean;
+  setIsMenuOpen: (isMenuOpen: boolean) => void;
 };
 
-export const NavigationComponent = ({ isOpen }: props) => {
+export const NavigationComponent = ({ isMenuOpen, setIsMenuOpen }: props) => {
   const dispatch = useAppDispatch();
-  const { user, dictionary } = useAppSelector((state) => {
+  const { user, dictionary, isAuthorized } = useAppSelector((state) => {
     return {
       user: state.user,
       dictionary: state.language.lang,
+      isAuthorized: state.user.isAuthorized,
     };
   });
   const firstNamesLetter = user.name ? user.name[0].toUpperCase() : null;
@@ -80,19 +81,32 @@ export const NavigationComponent = ({ isOpen }: props) => {
     }
   };
 
-  return (
-    <Wrapper className={isOpen ? Styles.ACTIVE : ''}>
-      <NavLink to="search">
-        <ItemContainer>
-          <HandySvg src={searchIcon} width="26" height="26" />
-          <p>{dictionary.header.Search}</p>
-        </ItemContainer>
-      </NavLink>
+  const closeMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-      <NewBoardButton onClick={() => dispatch(setStatus('new_board'))}>
-        <HandySvg src={plus} width={SvgSizes.SMALL} height={SvgSizes.SMALL} />
-        <p>{dictionary.boardsPage['New Board']}</p>
-      </NewBoardButton>
+  return (
+    <Wrapper className={isMenuOpen ? Styles.ACTIVE : ''}>
+      {isAuthorized && (
+        <>
+          <NavLink to="search" onClick={closeMenu}>
+            <ItemContainer>
+              <HandySvg src={searchIcon} width="26" height="26" />
+              <p>{dictionary.header.Search}</p>
+            </ItemContainer>
+          </NavLink>
+
+          <NewBoardButton
+            onClick={() => {
+              closeMenu();
+              dispatch(setStatus('new_board'));
+            }}
+          >
+            <HandySvg src={plus} width={SvgSizes.SMALL} height={SvgSizes.SMALL} />
+            <p>{dictionary.boardsPage['New Board']}</p>
+          </NewBoardButton>
+        </>
+      )}
 
       {switchOptions.map((item) => {
         return (
@@ -103,29 +117,44 @@ export const NavigationComponent = ({ isOpen }: props) => {
             </TextContainer>
             <OptionsContainer onClick={(e) => handleOption((e.target as HTMLElement).innerText)}>
               {item.options.map((option, index) => {
-                return <li key={index}>{option}</li>;
+                return (
+                  <li key={index} onClick={closeMenu}>
+                    {option}
+                  </li>
+                );
               })}
             </OptionsContainer>
           </Switch>
         );
       })}
+      {isAuthorized ? (
+        <>
+          <LogOut
+            onClick={() => {
+              closeMenu();
+              dispatch(authorizationSwitch());
+              deleteCookie(getCookie('TASKBAN_USER_TOKEN') as string);
+              deleteValueFromLocalStorage(localData);
+            }}
+          >
+            {dictionary.header.LogOut}
+          </LogOut>
 
-      <LogOut
-        onClick={() => {
-          dispatch(authorizationSwitch());
-          deleteCookie(getCookie('TASKBAN_USER_TOKEN') as string);
-          deleteValueFromLocalStorage(localData);
-        }}
-      >
-        {dictionary.header.LogOut}
-      </LogOut>
-
-      <NavLink to="profile">
-        <User>
-          <p>{dictionary.profile.EditProfile}</p>
-          <UserAvatar>{firstNamesLetter}</UserAvatar>
-        </User>
-      </NavLink>
+          <NavLink to="profile" onClick={closeMenu}>
+            <User>
+              <p>{dictionary.profile.EditProfile}</p>
+              <UserAvatar>{firstNamesLetter}</UserAvatar>
+            </User>
+          </NavLink>
+        </>
+      ) : (
+        <ButtonContainer className={isMenuOpen ? Styles.ACTIVE : ''} onClick={closeMenu}>
+          <SignIn onClick={() => dispatch(setStatus('login'))}>{dictionary.header.SignIn}</SignIn>
+          <SignUp onClick={() => dispatch(setStatus('registration'))}>
+            {dictionary.header.SignUp}
+          </SignUp>
+        </ButtonContainer>
+      )}
     </Wrapper>
   );
 };
