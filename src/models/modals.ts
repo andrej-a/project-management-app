b0156bf8021fe49338bf670b67e4e3703ecb9819
Lizaveta Petrova <goldglaider@gmail.com>
2022-12-05 22:27:32 +0300

    feat: add fetchError

--- src/models/modals.ts
+++ src/models/modals.ts
@@ -9,7 +9,8 @@ export type ModalsState = {
     | 'delete_item'
     | 'update_card'
     | 'update_board'
-    | 'view_card';
+    | 'view_card'
+    | 'fetch_error';
   warningMessage: string;
   loadingState: 'loaded' | 'loading';
   deletingValue: string;
